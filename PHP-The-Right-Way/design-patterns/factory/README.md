c81e6123fcc7a968ca24d014f07dd75350c163af
SenCaoKing <1134349927@qq.com>
2018-05-28 16:50:24 +0800

    设计模式之工厂模式 - 类封装

--- PHP-The-Right-Way/design-patterns/factory/README.md
+++ PHP-The-Right-Way/design-patterns/factory/README.md
@@ -1,1 +1,3 @@
 #### factory.php 工厂模式 示例
+
+#### factory_class.php 工厂模式之类封装
---
+++ PHP-The-Right-Way/design-patterns/factory/factory_class.php
@@ -0,0 +1,38 @@
+﻿<?php
+class Factory
+{
+    public static function build($type)
+    {
+        $class = 'Format' . $type;
+        if (!class_exists($class)) {
+            throw new Exception('Missing format class.');
+        }
+        return $class;
+    }
+}
+
+interface FormatInterface {}
+
+class FormatString implements FormatInterface {}
+class FormatNumber implements FormatInterface {}
+
+try {
+    $string = Factory::build('String');
+} catch(Exception $e) {
+    echo $e->getMessage();
+}
+
+try {
+    $number = Factory::build('Number');
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+try {
+    $number = Factory::build('Array');
+} catch (Exception $e) {
+    echo $e->getMessage(); // Missing format class.
+}
+
+
+?>
